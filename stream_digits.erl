d9d8ff6c251cc2cf57710855f8f3dc56bd3872fc
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-19 11:35:57 +0200

    digits: encode pi in binary

--- stream_digits.erl
+++ stream_digits.erl
@@ -5,6 +5,7 @@
 %% See LICENSE for licensing information.
 -module(stream_digits).
 -mode('compile').
+-compile([debug_info]).
 
 %% stream_digits: produce binary data on stdout.
 
@@ -19,13 +20,25 @@
 
 %% API
 
-main (_) ->
-    loop(#{yielder => fun sqrt/1
-          ,data => 1
-          ,succ => fun sqrt_succ/1
-          }).
+main ([Arg]) ->
+    loop(init(Arg)).
 
 %% Internals
+
+init ("sqrt") ->
+    #{yielder => fun sqrt/1
+     ,data => 1
+     ,succ => fun sqrt_succ/1
+     };
+init (File) ->
+    case file:open(File, [read, binary, raw]) of
+        {ok, Dev} ->
+            #{yielder => fun translate/1
+             ,data => Dev
+             ,succ => fun id/1
+             };
+        _Error -> fail("~s: ~p", [File, _Error])
+    end.
 
 loop (State=#{yielder := Yield
              ,succ := Succ
@@ -33,15 +46,33 @@ loop (State=#{yielder := Yield
     emit(Yield(State)),
     loop(Succ(State)).
 
-emit (Term) ->
-    Bin = term_to_binary(Term),
-    io:format("~s", [Bin]).
+emit (Float) when is_float(Float) ->
+    Bin = term_to_binary(Float),
+    io:format("~s", [Bin]);
+emit (Byte) when is_integer(Byte), Byte =< 0 ->
+    Byte + 256;
+emit (Byte) when is_integer(Byte), Byte > 255 ->
+    Byte - 256;
+emit (Byte) when is_integer(Byte), Byte < 256 ->
+    io:format("~s", [[Byte]]).
 
+
+id (A) -> A.
 
 sqrt (#{data := N}) ->
     math:sqrt(N).
-
 sqrt_succ (State) ->
     State#{data => 1 + maps:get(data, State)}.
+
+translate (#{data := Device}) ->
+    case file:read(Device, 2) of
+        {ok, <<High, Low>>} ->
+            10 * (High - $0) + Low - $0;
+        _ -> halt(0)
+    end.
+
+fail (Fmt, Args) ->
+    io:format(standard_error, Fmt++"\n", Args),
+    halt(1).
 
 %% End of Module.
